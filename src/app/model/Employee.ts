8e7373a8187424445f88c98c9020b5b32eb3ae3a
Abbas Raza <abbasrazaa02@gmail.com>
2025-01-19 19:39:33 +0500

    employee page created

---
+++ src/app/model/Employee.ts
@@ -0,0 +1,23 @@
+export class Employee {
+  employeeId: number;
+  employeeName: string;
+  contactNo: string;
+  emailId: string;
+  deptId: number;
+  password: string;
+  gender: string;
+  role: string;
+  createDate: Date;
+
+  constructor() {
+    this.employeeId = 0;
+    this.employeeName = '';
+    this.contactNo = '';
+    this.emailId = '';
+    this.deptId = 0;
+    this.gender = '';
+    this.password = '';
+    this.role = '';
+    this.createDate = new Date();
+  }
+}
