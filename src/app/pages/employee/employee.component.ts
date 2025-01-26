65a664235437101458c7b5fcead87931840b7a06
Abbas Raza <abbasrazaa02@gmail.com>
2025-01-26 18:48:34 +0500

    added Employee service

--- src/app/pages/employee/employee.component.ts
+++ src/app/pages/employee/employee.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MasterService } from '../../services/master.service';
 import { Employee, IApiResponse, IChildDept, IParentDept } from '../../model/Employee';
 import { FormsModule } from '@angular/forms';
+import { EmployeeService } from '../../services/employee.service';
 
 @Component({
   selector: 'app-employee',
@@ -15,11 +16,19 @@ export class EmployeeComponent implements OnInit {
   deptId: number = 0;
 
   employeeObj: Employee = new Employee();
-
+  employeeList: Employee[] = [];
   masterService = inject(MasterService);
-
+  empService = inject(EmployeeService)
+ 
   ngOnInit(): void {
     this.getParentDeptList();
+    this.getEmployees();
+  }
+
+  getEmployees() {
+    this.empService.getEmployee().subscribe((res: Employee[]) => {
+      this.employeeList = res;
+    }); 
   }
 
   getParentDeptList() {
@@ -32,5 +41,28 @@ export class EmployeeComponent implements OnInit {
     this.masterService.getChildDeptByParentId(this.deptId).subscribe((res: IApiResponse) => {
       this.childDeptList = res.data;
     })
-  }  
+  }
+
+  onSaveEmp(){
+    debugger;
+    this.empService.createNewEmployee(this.employeeObj).subscribe((res: Employee)=>{
+      debugger;
+        alert("Employee Creation Success") 
+    }, error=>{
+      alert('Error From API')
+    })
+  }
+
+  onDelete(id: number){
+    const result = confirm('Are you surely want to Delete')
+    if(result){
+      this.empService.deleteEmpById(id).subscribe((res:Employee)=>{
+        debugger;
+        alert("Employee deletion Success")
+        this.getEmployees();
+      },error=>{
+        alert('Error Form API')
+      })
+    }
+  }
 }
---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee, IApiResponse } from '../model/Employee';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  constructor(private http: HttpClient) { }
+
+  createNewEmployee(obj:Employee){
+    return this.http.post<Employee>(
+      "https://projectapi.gerasim.in/api/EmployeeManagement/CreateEmployee",obj)
+    }
+  getEmployee() {
+    return this.http.get<Employee[]>(
+      "https://projectapi.gerasim.in/api/EmployeeManagement/GetAllEmployees"
+    )
+  }
+  deleteEmpById(id: number) {
+    return this.http.delete<Employee>(
+      "https://projectapi.gerasim.in/api/EmployeeManagement/DeleteEmployee"+id
+    )
+  }
+}
