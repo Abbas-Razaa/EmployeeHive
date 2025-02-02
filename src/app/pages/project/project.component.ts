2a355a797f5f192ead50872efb33bb1b95d0a314
Abbas Raza <abbasrazaa02@gmail.com>
2025-02-02 23:24:49 +0500

    added project form

--- src/app/pages/project/project.component.ts
+++ src/app/pages/project/project.component.ts
@@ -1,12 +1,30 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-project',
-  imports: [NgIf],
+  imports: [NgIf,ReactiveFormsModule],
   templateUrl: './project.component.html',
   styleUrl: './project.component.css'
 })
 export class ProjectComponent {
   currentView: string = "List";
+  projectForm: FormGroup = new FormGroup({});
+
+  constructor(){
+    this.initializeForm();
+  }
+
+  initializeForm(){
+    this.projectForm = new FormGroup({
+      projectId: new FormControl(0),
+      projectName: new FormControl(''),
+      clientName: new FormControl(''),
+      startDate: new FormControl(''),
+      leadByEmpId: new FormControl(''),
+      contactNo: new FormControl(''),
+      emailId: new FormControl('')
+    });
+  }
 }
