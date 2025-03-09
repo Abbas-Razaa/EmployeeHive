eef61eebe1df2c94982c326c7a5d1a47204c2d23
Abbas Raza <abbasrazaa02@gmail.com>
2025-03-09 23:07:39 +0500

    project

--- src/app/model/Employee.ts
+++ src/app/model/Employee.ts
@@ -51,3 +51,25 @@ export interface Project {
   emailId: string;
   employeeName: string;
 }
+
+export class Project {
+  projectId: number;
+  projectName: string;
+  clientName: string;
+  startDate: Date;
+  leadByEmpId: number;
+  contactPerson: string;
+  contactNo: string;
+  emailId: string;
+
+  constructor() {
+    this.projectId = 0;
+    this.projectName = '';
+    this.clientName = '';
+    this.startDate = new Date();
+    this.leadByEmpId = 0;
+    this.contactPerson = '';
+    this.contactNo = '';
+    this.emailId = '';
+  }
+}
